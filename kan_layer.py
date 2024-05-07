8e1e6235134d40b2d50ed544177f8685ceccc893
Zhang Yanbo <Zhang.Yanbo@asu.edu>
2024-05-07 13:16:44 -0400

    add documents

--- kan_layer.py
+++ kan_layer.py
@@ -4,6 +4,20 @@ import torch.nn.functional as F
 
 
 def phi(x, w1, w2, b1, b2, n_sin):
+    """
+    phi function that integrates sinusoidal embeddings with MLP layers.
+
+    Args:
+        x (torch.Tensor): Input tensor.
+        w1 (torch.Tensor): Weight matrix for the first linear transformation.
+        w2 (torch.Tensor): Weight matrix for the second linear transformation.
+        b1 (torch.Tensor): Bias vector for the first linear transformation.
+        b2 (torch.Tensor): Bias vector for the second linear transformation.
+        n_sin (int): Number of sinusoidal functions to generate.
+
+    Returns:
+        torch.Tensor: Transformed tensor.
+    """
     omega = (2 ** torch.arange(0, n_sin)).float().reshape(-1, 1)
     omega_x = F.linear(x, omega, bias=None)
     x = torch.cat([x, torch.sin(omega_x), torch.cos(omega_x)], dim=-1)
@@ -15,7 +29,25 @@ def phi(x, w1, w2, b1, b2, n_sin):
 
 
 class KANLayer(nn.Module):
+    """
+    A layer in a Kolmogorov–Arnold Networks (KAN).
+
+    Attributes:
+        dim_in (int): Dimensionality of the input.
+        dim_out (int): Dimensionality of the output.
+        fcn_hidden (int): Number of hidden units in the feature transformation.
+        fcn_n_sin (torch.tensor): Number of sinusoidal functions to be used in phi.
+    """
     def __init__(self, dim_in, dim_out, fcn_hidden=32, fcn_n_sin=3):
+        """
+        Initializes the KANLayer with specified dimensions and sinusoidal function count.
+        
+        Args:
+            dim_in (int): Dimension of the input.
+            dim_out (int): Dimension of the output.
+            fcn_hidden (int): Number of hidden neurons in the for the learned non-linear transformation.
+            fcn_n_sin (int): Number of sinusoidal embedding frequencies.
+        """
         super().__init__()
         self.W1 = nn.Parameter(torch.randn(dim_in, dim_out, fcn_hidden, 1+fcn_n_sin*2))
         self.W2 = nn.Parameter(torch.randn(dim_in, dim_out, 1, fcn_hidden))
@@ -37,6 +69,15 @@ class KANLayer(nn.Module):
         nn.init.zeros_(self.B2)
     
     def map(self, x):
+        """
+        Maps input tensor x through phi function in a vectorized manner.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Output tensor after mapping through phi.
+        """
         F = torch.vmap(
             # take dim_in out, -> dim_in x (dim_out, *)(1)
             torch.vmap(phi, (None, 0, 0, 0, 0, None), 0), # take dim_out out, -> dim_out x (*)
@@ -45,6 +86,15 @@ class KANLayer(nn.Module):
         return F(x.unsqueeze(-1), self.W1, self.W2, self.B1, self.B2, self.fcn_n_sin).squeeze(-1)
 
     def forward(self, x):
+        """
+        Forward pass of the KANLayer.
+
+        Args:
+            x (torch.Tensor): Input tensor.
+
+        Returns:
+            torch.Tensor: Summed output after mapping each dimensions through phi.
+        """
         if len(x.shape) == 1:
             x = x.unsqueeze(0)
             
@@ -57,6 +107,16 @@ class KANLayer(nn.Module):
         return phis.sum(dim=1)
     
     def take_function(self, i, j):
+        """
+        Returns a phi function specific to the (i, j)-th elements of parameters.
+
+        Args:
+            i (int): Row index in parameter tensors.
+            j (int): Column index in parameter tensors.
+
+        Returns:
+            function: A function that computes phi for specific parameters.
+        """
         def activation(x):
             return phi(x, self.W1[i, j], self.W2[i, j], self.B1[i, j], self.B2[i, j], self.fcn_n_sin)
         return activation
